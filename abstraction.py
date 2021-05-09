369c1cc0a39f06328fcc24ee0416d6be49cc2f05
Zhanyl Umetalieva <77604986+zhanyll@users.noreply.github.com>
2021-05-09 13:22:45 +0600

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, age, points):
+        self.name = name
+        self.age = age
+        self.points = points
+
+class Face:
+    def __init__(self, eyes, nose, mouth, eyebrows):
+        self.eyes = eyes
+        self.nose = nose
+        self.mouth = mouth
+        self.eyebrows = eyebrows
